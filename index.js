042d2e0c551a5db9e1f3ca63241ba40f12c4ba7e
Zak <zak.johansson@gmail.com>
2023-03-29 13:42:18 -0700

    create mock data

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-let myLibrary = [];
+let myLibrary = [
+  {
+    title: 'Dune',
+    author: 'Frank Herbert',
+    status: "Read"
+  },
+  {
+    title: 'Fellowship of the Rings',
+    author: "J.R.R Tolkien",
+    status: 'Not Read'
+  },
+  {
+    title: 'Red Shirts',
+    author: 'John Scalzi',
+    status: 'Not Read'
+  }
+];
 
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  return myLibrary.push(e);
 }
+
+function displayBook(array) {
+}
+
+
+displayBook(myLibrary);
